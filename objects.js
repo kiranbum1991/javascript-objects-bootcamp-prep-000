8e9c2a6e0dc3d49772c81e1fb3c9b8d50b34c7f3
thomas lombardo <tomlom1984@hotmail.co.uk>
2019-05-25 11:33:15 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,8 +4,8 @@ var playlist = {key: "value"};
 
 //Create a function updatePlaylist that accepts three parameters: the playlist (an object), an artist name (a string), and a song title. The body of the function should add the song and artist as a key-value pair to the playlist object. The function should return the whole playlist.
 
-function updatePlaylist(obj, key, value) {
- return Object.assign({}, obj, {[key]: value});
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
 }
-
 //Create a function removeFromPlaylist that accepts two arguments (the playlist object and the artist name). The body of the function should delete the key-value pair from the playlist and return the updated playlist.
